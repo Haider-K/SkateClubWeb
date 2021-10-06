ff47443dd012caac616298e18972f73a86f9ee44
HotwheelH <haiderkhan343@gmail.com>
2021-10-06 15:00:17 +0100

    Added model validation to DTO

--- SkateClubWeb/Models/SendMailDto.cs
+++ SkateClubWeb/Models/SendMailDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,13 @@ namespace SkateClubWeb.Models
 {
     public class SendMailDto
     {
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string Subject { get; set; }
 
+        [Required]
         public string Message { get; set; }
     }
 }
