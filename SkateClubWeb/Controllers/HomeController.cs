c7a5d2d99950d4ff8c736cb051491d46b3c61e8f
HotwheelH <haiderkhan343@gmail.com>
2021-10-06 17:08:28 +0100

    Added code for send mail to be used on the contact us page

--- SkateClubWeb/Controllers/HomeController.cs
+++ SkateClubWeb/Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -45,6 +47,49 @@ namespace SkateClubWeb.Controllers
         [HttpPost]
         public IActionResult ContactUs(SendMailDto sendMailDto)
         {
+            if (!ModelState.IsValid) return View();
+
+            try
+            {
+                MailMessage mail = new MailMessage();
+                //my email address
+                mail.From = new MailAddress("SkateClubWeb@gmail.com");
+
+                //To Email Address
+                mail.To.Add("haiderkhan343@gmail.com");
+
+                mail.Subject = sendMailDto.Subject;
+
+                mail.IsBodyHtml = true;
+
+                string content = "Name : " + sendMailDto.Name;
+                content += "<br/> Message : " + sendMailDto.Message;
+
+                mail.Body = content;
+
+                //create SMTP instant
+                SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+
+                //network credential
+                NetworkCredential networkCredential = new NetworkCredential("SkateClubWeb@gmail.com", "Haider12.");
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = networkCredential;
+                smtpClient.Port = 587;
+                smtpClient.EnableSsl = true;
+                smtpClient.Send(mail);
+
+                ViewBag.Message = "Mail Send";
+
+                //Create the mail state
+                ModelState.Clear();
+
+            } catch (Exception ex)
+            {
+                //If any error occured it will show this message
+                ViewBag.Message = ex.Message.ToString();
+            }
+
+
             return View();
         }
     }
