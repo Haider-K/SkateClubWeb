13c84632318bb3461e5c37ebbf84981571bb1bba
HotwheelH <haiderkhan343@gmail.com>
2021-10-06 13:13:09 +0100

    created SendMailDto (Data Transfer Object) and initialised properties (Name, Subject and Message)

---
+++ SkateClubWeb/Models/SendMailDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkateClubWeb.Models
+{
+    public class SendMailDto
+    {
+        public string Name { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Message { get; set; }
+    }
+}
